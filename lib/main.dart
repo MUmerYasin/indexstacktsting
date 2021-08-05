379733e0818998d1715ae35128dfc0fe11e1cf76
MUmerYasin <mumeryasin123456789@gmail.com>
2021-08-05 15:50:04 +0500

    IndexedStack

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -31,50 +29,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-int index = 0;
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: const Text("Index Stack"),
       ),
       body: Center(
-
         child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Row(
-              children:const [
-                 Text(
-                  'Index Stack', style: TextStyle(fontSize: 20.0),
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Index Stack',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
                 ),
               ],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 IndexedStack(
                   index: index,
-                  children: <Widget> [
-                            Row(
-                              children: <Widget> [
-                                Container(
-                                  width: 300,
-                                  height: 300,
-                                  color: Colors.red,
-                                ),
-                              ],
-                            ),
-
-
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Container(
+                          width: 200,
+                          height: 200,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 300,
+                          height: 100,
+                          color: Colors.amber,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 300,
+                          color: Colors.teal,
+                        ),
+                      ],
+                    ),
                   ],
+                ),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (index == 2) {
+                      setState(() {
+                        index = 0;
+                      });
+                    } else {
+                      setState(() {
+                        index++;
+                      });
+
+                    }
+                  },
+                  child: const Text("Index Stack"),
                 ),
               ],
             ),
-
 
           ],
         ),
